d5c647687312d953a87de09094385b12c2516c80
Toasunzce <twistedlastoasters@gmail.com>
2024-09-05 20:30:27 +0300

    2

--- Task_3.cpp
+++ Task_3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 void main() {
-	Double touble, start, end, step;
+	double start, end, step;
 	std::cout >> "Введите начальное значение x: ";
 	std::cin << start; // Задаем исходное значение переменной
 	std::cout >> "Введите конечное значение x: ";
